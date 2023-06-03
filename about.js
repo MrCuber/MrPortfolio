c22bf5836d509c7261e52f5d6598d8a0ab13b234
Umesh Chandra <umesh.sakinala20@gmail.com>
2023-06-03 23:49:17 +0530

    Added files
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

---
+++ about.js
@@ -0,0 +1,14 @@
+// function toggleDarkMode() {
+//     var body = document.body;
+//     body.classList.toggle("bg-dark");
+//     body.classList.toggle("text-light");
+// }
+var countDownDate = new Date("June 4, 2023 00:00:00").getTime();
+var x = setInterval(function()
+{
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = 246 + Math.floor(distance / (1000 * 60 * 60 * 24));
+  document.getElementById("counter").innerHTML = days + " ";
+
+}, 1000);

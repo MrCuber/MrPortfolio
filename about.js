5307bec2bc84dac3a576cbb5dd4ceaeda3b0718a
Umesh Chandra <umesh.sakinala20@gmail.com>
2023-06-04 07:46:27 +0530

    Fixed the Error in Streak
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

--- about.js
+++ about.js
@@ -7,8 +7,7 @@ var countDownDate = new Date("June 4, 2023 00:00:00").getTime();
 var x = setInterval(function()
 {
   var now = new Date().getTime();
-  var distance = countDownDate - now;
+  var distance = now-countDownDate;
   var days = 246 + Math.floor(distance / (1000 * 60 * 60 * 24));
   document.getElementById("counter").innerHTML = days + " ";
-
 }, 1000);
